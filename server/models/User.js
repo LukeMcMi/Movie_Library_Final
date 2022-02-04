5ed53981ee5fb8171c9c584f9bfd02d4bf839bb1
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-04 14:00:17 +1100

    added server components.

--- server/models/User.js
+++ server/models/User.js
@@ -1,57 +1,88 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 
-// import schema from Book.js
-const bookSchema = require('./Book');
-
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+        type: String,
+        trim: true,
+        unique: 1
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        minglength: 5
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
-    savedBooks: [bookSchema],
-  },
-  // set this to use virtual below
-  {
-    toJSON: {
-      virtuals: true,
+    lastname: {
+        type: String,
+        maxlength: 50
     },
-  }
-);
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String,
+    },
+    tokenExp: {
+        type: Number
+    }
+})
 
-// hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
 
-  next();
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
 });
 
-// custom method to compare and validate password for logging in
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
-  return this.savedBooks.length;
-});
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secret')
 
-const User = model('User', userSchema);
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user);
+    })
+}
 
-module.exports = User;
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secret', function (err, decode) {
+        user.findOne({ "_id": decode, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User }
