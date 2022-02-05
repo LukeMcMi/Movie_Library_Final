2f70f1b4fb017d81717f847583a7a6988efaac98
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-05 11:36:54 +1100

    changes structure of server.

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,26 +1,33 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const express = require('express');
+const router = express.Router();
+const { Comment } = require("../models/Comment");
+const { auth } = require("../middleware/auth");
 
-const commentSchema = mongoose.Schema({
-    writer: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    postId: {
-        type: String,
-    },
-    responseTo: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    content: {
-        type: String
-    }
+router.post("/saveComment", auth, (req, res) => {
 
-},
-    { timestamps: true })
+    const comment = new Comment(req.body)
 
+    comment.save((err, comment) => {
+        console.log(err)
+        if (err) return res.json({ success: false, err })
 
-const Comment = mongoose.model('Comment', commentSchema);
+        Comment.find({ '_id': comment._id })
+            .populate('writer')
+            .exec((err, result) => {
+                if (err) return res.json({ success: false, err })
+                return res.status(200).json({ success: true, result })
+            })
+    })
+})
 
-module.exports = { Comment }
+router.post("/getComments", (req, res) => {
+
+    Comment.find({ "postId": req.body.movieId })
+        .populate('writer')
+        .exec((err, comments) => {
+            if (err) return res.status(400).send(err)
+            res.status(200).json({ success: true, comments })
+        })
+});
+
+module.exports = router;
