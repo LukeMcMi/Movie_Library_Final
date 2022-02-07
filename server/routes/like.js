4ec4051976d0915bfbbbeb9c616412b8c04e23b8
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-07 23:41:01 +1100

    code

--- server/routes/like.js
+++ server/routes/like.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Like = require("../models/Like");
 const Dislike = require("../models/Dislike");
 
-
 router.post("/getLikes", (req, res) => {
 
     let variable = {}
