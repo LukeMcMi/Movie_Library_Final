eef064b780025e89d0b2355cad76ba79804a187b
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-08 09:14:05 +1100

    changed cases

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { Favorite } = require("../models/Favorite");
+const { Favorite } = require("../models/favorite");
 
 
 router.post("/favoriteNumber", (req, res) => {
--- server/routes/like.js
+++ server/routes/like.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Like = require("../models/Like");
-const Dislike = require("../models/Dislike");
+const Like = require("../models/like");
+const Dislike = require("../models/dislike");
 
 router.post("/getLikes", (req, res) => {
 
