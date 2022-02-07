147883fac0a9bb46bae5f7fa1dc809963e50fe4a
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-07 22:11:51 +1100

    index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const config = require("/config/key");
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
 mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
