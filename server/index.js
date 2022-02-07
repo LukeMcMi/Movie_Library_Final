bd66853d27d2de9406d0075de7b81f3ff2722fa6
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-07 22:06:35 +1100

    server change

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const config = require("./config/key");
+const config = require("/config/key");
 
 const mongoose = require("mongoose");
 mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/comment', require('./routes/comment'));
-app.use('/api/like', require('./routes/like'));
-app.use('/api/favorite', require('./routes/favorite'));
+app.use('/api/users', require('/routes/users'));
+app.use('/api/comment', require('/routes/comment'));
+app.use('/api/like', require('/routes/like'));
+app.use('/api/favorite', require('/routes/favorite'));
 
 // https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
