dfafb548445ff8992b4d814c948652d8af75adba
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-08 13:55:05 +1100

    code

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 
-import Reducer from './reducers';
+import Reducer from './utils/reducer';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
