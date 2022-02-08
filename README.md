16d21ff4883cf8186c1efc890c6f354e43d5a389
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-02-08 22:02:34 +1100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Movie Libaray
+# Movie Library
+
+  ![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+  
+  ## Description
+  This application allows users to view movie details, create an account and list favorites. Comments can be added to movies.
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Link](#link)
+  - [Image](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  Application is deployed on Heroku
+  
+  ## Usage
+  Application is used to record your favorite movies.
+  
+  ## License
+  
+  ![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+  <br />
+  This application is covered under the MIT license.
+
+  ## Contributing
+  Contact developer to contribute
+
+  ## Link
+  [Heroku_Link](https://movie-library-tmdb.herokuapp.com/)
+
+  ## Image
+  ![Image](assets/movie-library-tmdb.herokuapp.com_.png)
+
+  ## Questions
+  Contact developer if you experience issues.
+  
+  Find me on GitHub: [LukeMcMi](https://github.com/LukeMcMi)
+  
+  Email me if you have any questions: n/a
---
+++ assets/client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "./utils/auth";
+
+import LandingPage from "./pages/LandingPage/LandingPage.js.js.js";
+import LoginPage from "./pages/LoginPage/LoginPage.js.js.js";
+import RegisterPage from "./pages/RegisterPage/RegisterPage.js.js.js";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer"
+import MovieDetail from "./components/MovieDetail/MovieDetail"
+import FavoritePage from "./pages/FavoritePage/FavoritePage"
+
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 72px)' }}>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
+          <Route exact path="/favorite" component={Auth(FavoritePage, null)} />
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
